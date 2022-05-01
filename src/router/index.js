84d9b60d586f46316a1aa52f8fd8620e71d437f4
曲乐皓 <19373528@buaa.edu.cn>
2022-05-01 15:32:07 +0800

    GoodsWall

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import GoodsView from "@/views/GoodsView";
+import GoodsInfo from "@/components/GoodsInfo";
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/goods',
+    name: 'GoodsView',
+    component: GoodsView,
+    children:   {
+      path: '/goodsInfo',
+      name: 'GoodsInfo',
+      component: GoodsInfo
+    }
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
 const router = createRouter({
---
+++ src/views/GoodsView.vue
@@ -0,0 +1,134 @@
+<template>
+  <div style="display: none">
+    <img src="../assets/rabbit.png" ref="rabbit">
+  </div>
+  <div style="display: none">
+    <img src="../assets/bookmark.png" ref="bookmark">
+  </div>
+  <div style="display: none">
+    <img src="../assets/wall.png" ref="wall">
+  </div>
+  <canvas
+      ref="myCanvas"
+      id="myCanvas"
+      width="100"
+      height="200"
+      style="background: black; height: 700px; width: 1500px; "
+      @mousedown="canvasMouseDown"
+      @mousemove="canvasMouseMove"
+      @mouseup="canvasMouseUp"
+  ></canvas>
+
+</template>
+
+<script>
+import {ref} from "vue";
+import GoodsInfo from "@/components/GoodsInfo";
+export default {
+  name: "GoodsView",
+  components: [
+    GoodsInfo
+  ],
+  data() {
+    return {
+      images: {
+        "rabbit":null,
+        "bookmark": null,
+        'wall': null
+      },
+      sizes: {
+        'rabbit': {width:250, height: 250},
+        'bookmark': {width: 250, height: 250},
+        'wall': {width: 3000, height: 800},
+      },
+      offsets: {
+        "rabbit": {x:250, y:400},
+        "bookmark": {x:1000, y:200},
+        'wall': {x: -500, y: 0},
+      },
+      ctx: null,
+      myCanvas: null,
+      now: {
+        x:0,
+        y:0,
+      },
+      pos: {
+        x:0,
+        y:0
+      },
+      down: ref(false),
+    }
+  },
+
+  methods: {
+    canvasMouseDown(e) {
+      this.down = ref(true);
+      this.now = this.getCanvasMousePos(e);
+    },
+    canvasMouseMove(e) {
+      if (this.down) {
+        this.pos = this.getCanvasMousePos(e);
+        if (!((this.offsets['wall'].x >= -100 && this.pos.x > this.now.x) || (this.offsets['wall'].x <= -this.sizes['wall'].width + this.myCanvas.width && this.pos.x < this.now.x))) {
+          this.ctx.clearRect(0, 0, this.myCanvas.width, this.myCanvas.height)
+          this.offsetBack('wall');
+          this.offset('rabbit');
+          this.offset('bookmark');
+          this.draw('wall');
+          this.draw('rabbit');
+          this.draw('bookmark');
+          this.now = this.pos;
+        }
+      }
+    },
+    canvasMouseUp(e) {
+      this.down = ref(false);
+    },
+    getCanvasMousePos (e) {
+      const rect = this.myCanvas.getBoundingClientRect()
+      const x = ((e.clientX - rect.left))
+      const y = ((e.clientY - rect.top))
+      return {
+        x: x,
+        y: y
+      }
+    },
+    init() {
+      console.log('????')
+      this.myCanvas = this.$refs.myCanvas;
+      this.setupCanvas(this.myCanvas);
+      this.images['rabbit'] = this.$refs.rabbit;
+      this.images['bookmark'] = this.$refs.bookmark;
+      this.images['wall'] = this.$refs.wall;
+      this.draw('wall');
+      this.draw("rabbit");
+      this.draw('bookmark');
+    },
+    setupCanvas(myCanvas) {
+      let rect = myCanvas.getBoundingClientRect();
+      let scale = window.devicePixelRatio || 1;
+      myCanvas.width = Math.floor(rect.width * scale);
+      myCanvas.height = Math.floor(rect.height * scale);
+      this.ctx = myCanvas.getContext('2d');
+      this.ctx.scale(scale, scale);
+    },
+
+    draw(name) {
+      this.ctx.drawImage(this.images[name], this.offsets[name].x, this.offsets[name].y, this.sizes[name].width, this.sizes[name].height);
+    },
+    offset(name) {
+      this.offsets[name].x = this.offsets[name].x + (this.pos.x - this.now.x)
+    },
+    offsetBack(name) {
+      this.offsets[name].x = this.offsets[name].x + (this.pos.x - this.now.x) * 0.3
+    }
+  },
+  mounted() {
+    this.init()
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
