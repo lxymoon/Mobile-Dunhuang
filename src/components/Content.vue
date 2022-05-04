2cf58dc58b9c97f30817e1de7f6cf55f10216d24
lxy <1626593186@qq.com>
2022-05-04 16:08:50 +0800

    add animation

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,33 +1,39 @@
 <template>
   <div id="IndexContent" style="">
-    <div id="title">
+    <div id="title" class="cartoon">
       <div style="margin: auto;
                   text-align: center;
                   width:60%;
                   font-family: shike;
-                  font-size: 100px"
+                  font-size: 100px;
+                  animation: flipInX 4s;
+                  -webkit-animation: flipInX 4s"
       >乘风游梦</div>
       <div style="font-family: shike; font-size: 40px; color: white; margin-top: 20px">线上虚拟展</div>
     </div>
     <div id="bar">
-      <div id="1" style="display:inline-block; width: 20%; float: left">
+      <div id="1" style="display:inline-block;
+                                  width: 20%;
+                                  float: left;
+                                  -webkit-animation: fadeInDown 1s;
+	                                animation: fadeInDown 1s;">
         <a href="">
-          <img src="../assets/paper-1.png" style="height: 485px">
+          <img class="smaller" src="../assets/paper-1.png" style="height: 485px;transition:all 0.3s">
         </a>
       </div>
       <div id="2" style="display:inline-block; width: 20%; float: left;">
         <a href="">
-          <img src="../assets/paper-2.png" style="height: 470px">
+          <img class="smaller" src="../assets/paper-2.png" style="height: 470px">
         </a>
       </div>
       <div id="3" style="display:inline-block; width: 20%; float: left; margin-left: 15px">
-        <router-link to="/three">
-          <img src="../assets/paper-3.png" style="height: 470px">
-        </router-link>
+        <a href="">
+          <img class="smaller" src="../assets/paper-3.png" style="height: 470px">
+        </a>
       </div>
       <div id="4" style="display:inline-block; width: 20%; float: left; margin-left: 20px; margin-top: 7px">
         <router-link to="/goods" >
-          <img src="../assets/paper-4.png" style="height: 470px">
+          <img class="smaller" src="../assets/paper-4.png" style="height: 470px">
         </router-link>
         <!-- <span @click="toGoods"><img @click="toGoods" src="../assets/paper-4.png" style="height: 500px"></span> -->
         <!-- <router-view key="key" /> -->
@@ -124,5 +130,96 @@ export default {
   float: left;
   /*height: 500px;*/
   width: 25%
+}
+div.cartoon {
+
+}
+img.smaller {
+  /*width: 250px;*/
+  height: 470px;
+  /*max-height: v-bind();*/
+  /*max-width: 300px;*/
+  animation: flipInX 4s;
+  -webkit-animation: flipInX 4s
+}
+
+@-webkit-keyframes flipInX {
+  0% {
+    -webkit-transform: perspective(400px) rotateX(90deg);
+    transform: perspective(400px) rotateX(90deg);
+    opacity: 0
+  }
+
+  40% {
+    -webkit-transform: perspective(400px) rotateX(-10deg);
+    transform: perspective(400px) rotateX(-10deg)
+  }
+
+  70% {
+    -webkit-transform: perspective(400px) rotateX(10deg);
+    transform: perspective(400px) rotateX(10deg)
+  }
+
+  100% {
+    -webkit-transform: perspective(400px) rotateX(0);
+    transform: perspective(400px) rotateX(0);
+    opacity: 1
+  }
+}
+@keyframes flipInX {
+  0% {
+    -webkit-transform: perspective(400px) rotateX(90deg);
+    -ms-transform: perspective(400px) rotateX(90deg);
+    transform: perspective(400px) rotateX(90deg);
+    opacity: 0
+  }
+
+  40% {
+    -webkit-transform: perspective(400px) rotateX(-10deg);
+    -ms-transform: perspective(400px) rotateX(-10deg);
+    transform: perspective(400px) rotateX(-10deg)
+  }
+
+  70% {
+    -webkit-transform: perspective(400px) rotateX(10deg);
+    -ms-transform: perspective(400px) rotateX(10deg);
+    transform: perspective(400px) rotateX(10deg)
+  }
+
+  100% {
+    -webkit-transform: perspective(400px) rotateX(0);
+    -ms-transform: perspective(400px) rotateX(0);
+    transform: perspective(400px) rotateX(0);
+    opacity: 1
+  }
+}
+@-webkit-keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(-20px);
+    transform: translateY(-20px)
+  }
+
+  100% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    transform: translateY(0)
+  }
+}
+
+@keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(-20px);
+    -ms-transform: translateY(-20px);
+    transform: translateY(-20px)
+  }
+
+  100% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    -ms-transform: translateY(0);
+    transform: translateY(0)
+  }
 }
 </style>
